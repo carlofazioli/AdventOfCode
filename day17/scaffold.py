9b729525dd19336b10a45b4c5f575e3c98c16db3
CCarloFazioli <cfazioli@groupw.com>
2019-12-17 15:46:05 -0500

    starting to run the bot along the scaffold

--- day17/scaffold.py
+++ day17/scaffold.py
@@ -18,3 +18,23 @@ def render(screen):
 
 
 render(camera_out)
+
+move_routine = 'A,B,A,C,A,A,C,B,C,B\n'
+A = 'L,12,L,8,R,11,L,8,L,12\n'
+B = 'L,99,L,9,L,99,R,9\n'
+C = 'R,99,L,99,L,99\n'
+video_feed = 'n\n'
+
+part2_inp = []
+part2_inp += list(map(ord, list(move_routine)))
+part2_inp += list(map(ord, list(A)))
+part2_inp += list(map(ord, list(B)))
+part2_inp += list(map(ord, list(C)))
+part2_inp += list(map(ord, list(video_feed)))
+
+pc2 = IntCodeComputer(source='input.mod', buffered=True)
+result = pc2.run(part2_inp)
+print(result)
+result = result[0]
+print(''.join(list(map(chr, result))))
+
